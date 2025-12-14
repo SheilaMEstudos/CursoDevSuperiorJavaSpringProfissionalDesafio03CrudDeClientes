03d860245068dedef45d0e65f4fff34daafcb86a
Sheila <>
2025-12-13 21:32:36 -0300

    projeto inicial

---
+++ src/main/java/com/cursodevsuperior/cruddeclientes/CruddeclientesApplication.java
@@ -0,0 +1,13 @@
+package com.cursodevsuperior.cruddeclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddeclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddeclientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursodevsuperior/cruddeclientes/CruddeclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursodevsuperior.cruddeclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddeclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
